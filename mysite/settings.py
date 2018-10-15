49b34f54c4b3b4c13f30132c9e764cddb85b318d
leeahtiaring <dldkgus0707@naver.com>
2018-10-15 20:04:38 +0900

    Changed ...

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k_z!fbvpqk0i0!ry(sp$^nazsd-q89i@4ps62mi@p6tbckc0d0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
